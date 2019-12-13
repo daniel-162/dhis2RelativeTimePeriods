099135961b2b36ea4351ec8cdfe69f8ab86f9f72
daniel-162 <57469674+daniel-162@users.noreply.github.com>
2019-12-13 02:03:36 +0200

    Updates

---
+++ precache-manifest.27ad84cc47295c27e23c478999915bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed32737b6b5a2ad4fb5f55bd150fc92",
+    "url": "/daniel-162/dhis2RelativeTimePeriods/index.html"
+  },
+  {
+    "revision": "8f9e5ec5e9fc0981735c",
+    "url": "/daniel-162/dhis2RelativeTimePeriods/static/css/main.067f0f9c.chunk.css"
+  },
+  {
+    "revision": "fe8cd357196401909730",
+    "url": "/daniel-162/dhis2RelativeTimePeriods/static/js/2.6824a3d4.chunk.js"
+  },
+  {
+    "revision": "3a6bf107d5b3a9480d5472bf5167990d",
+    "url": "/daniel-162/dhis2RelativeTimePeriods/static/js/2.6824a3d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f9e5ec5e9fc0981735c",
+    "url": "/daniel-162/dhis2RelativeTimePeriods/static/js/main.3bf4591f.chunk.js"
+  },
+  {
+    "revision": "dfb8ede6a07510ad9561",
+    "url": "/daniel-162/dhis2RelativeTimePeriods/static/js/runtime-main.858562ab.js"
+  }
+]);
